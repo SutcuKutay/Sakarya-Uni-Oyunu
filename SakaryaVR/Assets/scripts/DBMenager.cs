142a2454e1b6c82ed66f7062700c890420b753f4
Kutay Sütçü <94748829+SutcuKutay@users.noreply.github.com>
2022-12-22 17:36:34 +0300

    Real Time Database + Makine + Ilahiyat Fakultesi
    "Firebase" klasörü yok. Manuel elle ekle.

---
+++ SakaryaVR/Assets/scripts/DBMenager.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Firebase;
+using Firebase.Database;
+using UnityEngine.UI;
+using TMPro;
+
+public class DBMenager : MonoBehaviour
+{
+    public DatabaseReference usersRef;
+    public TMP_InputField mailInput, passInput;
+    public string statu;
+    void Start()
+    {
+        StartCoroutine(Initilization());
+    }
+
+    private IEnumerator Initilization()
+    {
+        var task = FirebaseApp.CheckAndFixDependenciesAsync();
+        while (!task.IsCompleted)
+        {
+            yield return null;
+        }
+
+        if (task.IsCanceled || task.IsFaulted)
+        {
+            Debug.LogError("Database Hatasý: " + task.Exception);
+        }
+
+        var dependencyStatus = task.Result;
+
+        if (dependencyStatus == DependencyStatus.Available)
+        {
+            usersRef = FirebaseDatabase.DefaultInstance.GetReference("SAU");
+            Debug.Log("SAU veritabanina baglanildi.");
+
+        }
+
+        else
+        {
+            Debug.LogError("Database Hatasý: ");
+        }
+    }
+
+    public void GetData()
+    {
+        StartCoroutine(GetUser());
+    }
+
+    public IEnumerator GetUser()
+    {
+        var task = usersRef.Child("Kullanicilar").GetValueAsync();
+        while (!task.IsCompleted)
+        {
+            yield return null;
+        }
+
+        if (task.IsCanceled || task.IsFaulted)
+        {
+            Debug.LogError("Database Hatasý: " + task.Exception);
+        }
+
+        DataSnapshot snapshot = task.Result;
+
+        bool check = false;
+
+        foreach (DataSnapshot user in snapshot.Children)
+        {
+            if (user.Child("Email").Value.ToString() == mailInput.text)
+            {
+                check = true;
+            }
+            if (check)
+            {
+                if (user.Child("Sifre").Value.ToString() == passInput.text)
+                {
+                    statu = user.Child("Statu").Value.ToString();
+                    SceneManager.LoadScene("Game");
+                }
+                else
+                {
+                    check = false;
+                }
+            }
+
+        }
+        if (!check)
+        {
+            Debug.Log("Email ya da Þifre Hatalý!");
+        }
+    }
+}
--- SakaryaVR/Assets/scripts/MainMenu.cs
+++ SakaryaVR/Assets/scripts/MainMenu.cs
@@ -5,12 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void Play()
-    {
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        //SceneManager.LoadScene("LoginScene");
-    }
-
     public void Quit()
     {
         Application.Quit();
